37ed07e45b69dc109e814f01970e1803ec3fed92
Aswin-Nath <aswinnathte125@gmail.com>
2025-11-05 10:24:02 +0530

    Created the venv correctly and used

---
+++ HotelBookingSystem/app/database/mongo_connnection.py
@@ -0,0 +1,15 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+
+client = AsyncIOMotorClient(MONGO_URI)
+database = client[MONGO_DB_NAME]
+
+# Utility function to get db instance
+def get_database():
+    return database
